d4791ca2f72c1d2bc6e4f548e6efc76516045dc4
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-02-02 20:07:41 +0100

    documentation updates

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,11 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    adds two numbers using type annotation
+    Keyword arguments:
+    a -- float variable
+    b -- float variable
+    Return: sum of a and b as float
+    """
     return a + b
